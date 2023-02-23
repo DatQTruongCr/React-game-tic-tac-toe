a9a9ca8d4e6cbf0224b064759c7e38696f3f0fbf
TRUONG QUOC DAT <w.d.truongquocdat@gmail.com>
2023-02-23 16:56:12 +0700

    finish create state and show new value X when click

--- src/component/Square.js
+++ src/component/Square.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
             <div>
-                <button className="Square" onClick= {() => {
-                    console.log('click me');
-                }}> {this.props.value} </button> 
+                <button className="Square"
+                    onClick={() => {
+                        console.log('click me');
+                        this.setState({value: 'X'})
+                    }}> {this.state.value} </button>
             </div>
 
         )
