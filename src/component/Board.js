b9256397170282b0cd7fc4dad6c70f742e0a8ed4
TRUONG QUOC DAT <w.d.truongquocdat@gmail.com>
2023-02-24 16:14:32 +0700

    finish turn Play and get value step by step not check winner

--- src/component/Board.js
+++ src/component/Board.js
@@ -3,17 +3,58 @@ import Square from './Square'
 
 export default class Board
     extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+        this.turnBackArray = [];
+        this.checkPlayer = true;
+        this.checkBackStep = true;
+    }
+
+    turnPlay(array,indexShow,iconPlay) {
+        array[indexShow] = iconPlay;
+        this.checkPlayer = !this.checkPlayer;
+        this.checkBackStep = true;
+    }
+
+    handleClick(index) {
+        if (this.state.squares[index] === null) {
+            let newSquares = this.state.squares.slice();
+            this.turnBackArray = this.state.squares;
+            if (this.checkPlayer) {
+                this.turnPlay(newSquares,index,'X');
+            } else {
+                this.turnPlay(newSquares,index,'O');
+            }
+            this.setState({ squares: newSquares });
+        }
+    }
+
     renderSquare(i) {
         return (
             <div>
-                <Square value={i} />
+                <Square
+                    value={this.state.squares[i]}
+                    onClick={() => {
+                        this.handleClick(i);
+                    }} />
             </div>
         )
     }
 
+    Turnback(array) {
+        if (this.checkBackStep) {
+            this.setState({ squares: array });
+            this.checkPlayer = !this.checkPlayer;
+            this.checkBackStep = !this.checkBackStep;
+        }
+    }
+
     render() {
         return (
-            <div>
+            <>
                 <div className='board-behind'>
                     <div className='board-row'>
                         {this.renderSquare(0)}
@@ -28,13 +69,17 @@ export default class Board
                     </div>
 
                     <div className='board-row'>
-                        {this.renderSquare('X')}
+                        {this.renderSquare(6)}
                         {this.renderSquare(7)}
                         {this.renderSquare(8)}
                     </div>
                 </div>
 
-            </div>
+                <button onClick={() => {
+                    this.Turnback(this.turnBackArray);
+                }}>Re-Step</button>
+
+            </>
         )
     }
 
